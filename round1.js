51f5534abfc92c31ed97b0670d4ac92e57bc2ccc
gabrielforti <G@br13l_F0rt1>
2018-12-22 20:19:38 -0500

    working round 1

--- round1.js
+++ round1.js
@@ -1,257 +1,408 @@
 module.exports = [{
-    question: "What holiday movie sequel includes a cameo by Donald Trump?",
-    questionES: "¿Qué secuela de la película de vacaciones incluye un cameo de Donald Trump?",
-    choices: [
-        'Home Alone 2',
-        'Home Alone',
-        'Home Alone 3',
-        'Home Alone 4'
-    ],
-    answer: 'Home Alone 2',
-    img: 'trumphomealone2.png'
+  question: "What holiday movie sequel includes a cameo by Donald Trump?",
+  questionES: "¿Qué secuela de la película de vacaciones incluye un cameo de Donald Trump?",
+  choices: [
+    'Home Alone 2',
+    'Home Alone',
+    'Home Alone 3',
+    'Home Alone 4'
+  ],
+  answer: 'Home Alone 2',
+  img: 'trumphomealone2.png'
 }
-,{
-    question: "In the song 'Frosty the Snowman', what made Frosty come to life?",
-    choices: [
-        'An old silk hat',
-        'Magic',
-        'Christmas Miracle',
-        'A bright red scarf'
-    ],
-    answer: 'An old silk hat',
-    img: 'frosty.png'
+  , {
+  question: "In the song 'Frosty the Snowman', what made Frosty come to life?",
+  questionES: "En la canción 'Frosty the Snowman', ¿qué hizo que Frosty cobrara vida?",
+  choices: [
+    'An old silk hat',
+    'Magic',
+    'Christmas Miracle',
+    'A bright red scarf'
+  ],
+  choicesES: [
+    'Un viejo sombrero de seda',
+    'Mágico',
+    'Milagro de Navidad',
+    'Un pañuelo rojo brillante'
+  ],
+  answer: 'An old silk hat',
+  answerES: 'Un viejo sombrero de seda',
+  img: 'frosty.png'
 }
-,{
-    question: "What Christmas beverage is also known as 'milk punch?'",
-    choices: [
-        'Eggnog',
-        'Hot Cocoa',
-        'Milk punch',
-        'Baby Formula'
-    ],
-    answer: 'Eggnog',
-    img: 'eggnog.png'
+  , {
+  question: "What Christmas beverage is also known as 'milk punch?'",
+  questionES: "¿Qué bebida navideña también se conoce como 'ponche de leche?'",
+  choices: [
+    'Eggnog',
+    'Hot Cocoa',
+    'Milk punch',
+    'Baby Formula'
+  ],
+  choicesES: [
+    'Ponche de huevo',
+    'Chocolate caliente',
+    'Ponche de leche',
+    'Formula para bebé'
+  ],
+  answer: 'Eggnog',
+  answerES: 'Ponche de huevo',
+  img: 'eggnog.png'
 }
-,{
-    question: "What Christmas-themed ballet premiered in Saint Petersburg, Russia in 1892?",
-    choices: [
-        'The Nutcracker',
-        'The Twelve Days of Christmas',
-        'A Christmas Carol',
-        'The Rockettes'
-    ],
-    answer: 'The Nutcracker',
-    img: 'theater.png'
+  , {
+  question: "What Christmas-themed ballet premiered in Saint Petersburg, Russia in 1892?",
+  questionES: "¿Qué ballet de temática navideña se estrenó en San Petersburgo, Rusia, en 1892?",
+  choices: [
+    'The Nutcracker',
+    'The Twelve Days of Christmas',
+    'A Christmas Carol',
+    'The Rockettes'
+  ],
+  choicesES: [
+    'El cascanueces',
+    'Los doce días de navidad',
+    'Un villancico',
+    'Las Rockettes'
+  ],
+  answer: 'The Nutcracker',
+  answerES: 'El cascanueces',
+  img: 'theater.png'
 }
-,{
-    question: "What traditional Christmas decoration is actually a parasitic plant?",
-    choices: [
-        'Mistletoe',
-        'Garland',
-        'Scotch Pine',
-        'Wreath'
-    ],
-    answer: 'Mistletoe',
-    img: 'decoration.png'
+  , {
+  question: "What traditional Christmas decoration is actually a parasitic plant?",
+  questionES: "¿Qué decoración tradicional de Navidad es en realidad una planta parasitaria?",
+  choices: [
+    'Mistletoe',
+    'Garland',
+    'Scotch Pine',
+    'Wreath'
+  ],
+  choicesES: [
+    'Muérdago',
+    'Guirnalda',
+    'Pino albar',
+    'Guirnalda'
+  ],
+  answer: 'Mistletoe',
+  answerES: 'Muérdago',
+  img: 'decoration.png'
 }
-,{
-    question: "How many red ornaments are found on the tree?",
-    choices: [
-        '21',
-        '19',
-        '22',
-        '18'
-    ],
-    answer: '21',
-    img: 'howmanyred.png'
+  , {
+  question: "How many red ornaments are found on the tree?",
+  questionES: "¿Cuántos adornos rojos se encuentran en el árbol?",
+  choices: [
+    '21',
+    '19',
+    '22',
+    '18'
+  ],
+  answer: '21',
+  img: 'howmanyred.png'
 }
-,{
-    question: "What beverage company has been using Santa Claus in its advertising since 1931?",
-    choices: [
-        'Coca-Cola',
-        'Pepsi',
-        '7up',
-        'Schweppes'
-    ],
-    answer: 'Coca-Cola',
-    img: 'cola.png'
+  , {
+  question: "What beverage company has been using Santa Claus in its advertising since 1931?",
+  questionES: "¿Qué compañía de bebidas ha estado usando a Santa Claus en su publicidad desde 1931?",
+  choices: [
+    'Coca-Cola',
+    'Pepsi',
+    '7up',
+    'Schweppes'
+  ],
+  answer: 'Coca-Cola',
+  img: 'cola.png'
 }
-,{
-    question: "What well-known Christmas carol became the first song ever broadcast from space in 1965?",
-    choices: [
-        'Jingle Bells',
-        'Hark! The Herald Angels Sing',
-        'We wish you a Merry Christmas',
-        'Oh Holy Night'
-    ],
-    answer: 'Jingle Bells',
-    img: 'recordplayer.png'
+  , {
+  question: "What well-known Christmas carol became the first song ever broadcast from space in 1965?",
+  questionES: "¿Qué famoso villancico se convirtió en la primera canción que se emitió desde el espacio en 1965?",
+  choices: [
+    'Jingle Bells',
+    'Hark! The Herald Angels Sing',
+    'We wish you a Merry Christmas',
+    'Oh Holy Night'
+  ],
+  choicesES: [
+    'Jingle Bells',
+    '¡Escuchar con atención! El Herald Angels Sing ',
+    'Le deseamos una Feliz Navidad',
+    'Oh noche santa'
+  ],
+  answer: 'Jingle Bells',
+  answerES: 'Jingle Bells',
+  img: 'recordplayer.png'
 }
-,{
-    question: "What kind of Christmas does Elvis Presley sing about?",
-    choices: [
-        'Blue',
-        'White',
-        'Red',
-        'Snowy'
-    ],
-    answer: 'Blue',
-    img: 'elvis.png'
+  , {
+  question: "What kind of Christmas does Elvis Presley sing about?",
+  questionES: "¿Sobre qué tipo de Navidad canta Elvis Presley?",
+  choices: [
+    'Blue',
+    'White',
+    'Red',
+    'Snowy'
+  ],
+  choicesES: [
+    'Azul',
+    'Blanco',
+    'Rojo',
+    'Nevado'
+  ],
+  answer: 'Blue',
+  answerES: 'Azul',
+  img: 'elvis.png'
 }
-,{
-    question: "Which Christmas movie has been played more than any other?",
-    choices: [
-        "It's a Wonderful Life",
-        'A Christmas Story',
-        'Home Alone',
-        'Frosty the Snowman'
-    ],
-    answer: "It's a Wonderful Life",
-    img: 'wonderfullife.png'
+  , {
+  question: "Which Christmas movie has been played more than any other?",
+  questionES: "¿Qué película navideña se ha jugado más que ninguna otra?",
+  choices: [
+    "It's a Wonderful Life",
+    'A Christmas Story',
+    'Home Alone',
+    'Frosty the Snowman'
+  ],
+  choicesES: [
+    "Es una vida maravillosa",
+    'Una historia de Navidad',
+    'Solo en casa',
+    'Frosty el Muñeco de Nieve'
+  ],
+  answer: "It's a Wonderful Life",
+  answerES: "Es una vida maravillosa",
+  img: 'wonderfullife.png'
 }
-,{
-    question: "The Christmas story is in which book(s) of the bible?",
-    choices: [
-        "Matthew and Luke",
-        'All four of the Gospels',
-        'Only Luke',
-        'Only Matthew'
-    ],
-    answer: "Matthew and Luke",
-    img: 'christmas-bible.jpg'
+  , {
+  question: "The Christmas story is in which book(s) of the bible?",
+  questionES: "La historia de Navidad es en que libro (s) de la biblia?",
+  choices: [
+    "Matthew and Luke",
+    'All four of the Gospels',
+    'Only Luke',
+    'Only Matthew'
+  ],
+  choicesES: [
+    "Mateo y Lucas",
+    'Los cuatro evangelios',
+    'Sólo Luke',
+    'Solo Mateo'
+  ],
+  answer: "Matthew and Luke",
+  answerES: "Mateo y Lucas",
+  img: 'christmas-bible.jpg'
 }
-,{
-    question: "What animated 2004 film is about a train that carries kids to the North Pole on Christmas Eve?",
-    choices: [
-        "The Polar Express",
-        'Rudolph the Red-Nosed Reindeer',
-        'Elf',
-        'The Santa Clause'
-    ],
-    answer: "The Polar Express",
-    img: 'thepolarexpress.png'
+  , {
+  question: "What animated 2004 film is about a train that carries kids to the North Pole on Christmas Eve?",
+  questionES: "¿Qué película animada de 2004 trata sobre un tren que lleva a los niños al Polo Norte en la víspera de Navidad?",
+  choices: [
+    "The Polar Express",
+    'Rudolph the Red-Nosed Reindeer',
+    'Elf',
+    'The Santa Clause'
+  ],
+  choicesES: [
+    "El expreso Polar",
+    'Rudolph el reno de nariz roja',
+    'Duende',
+    'La santa cláusula'
+  ],
+  answer: "The Polar Express",
+  answerES: "El expreso Polar",
+  img: 'thepolarexpress.png'
 }
-,{
-    question: "What film is about a father who vows to get his son a Turbo Man action figure for Christmas?",
-    choices: [
-        "Jingle All the Way",
-        'Miracle on 34th Street',
-        'A Christmas Story',
-        'A Christmas Carol'
-    ],
-    answer: "Jingle All the Way",
-    img: 'jinglealltheway.png'
+  , {
+  question: "What film is about a father who vows to get his son a Turbo Man action figure for Christmas?",
+  questionES: "¿Qué película trata sobre un padre que jura conseguirle a su hijo una figura de acción de Turbo Man para Navidad?",
+  choices: [
+    "Jingle All the Way",
+    'Miracle on 34th Street',
+    'A Christmas Story',
+    'A Christmas Carol'
+  ],
+  choicesES: [
+    "Resuena todo el camino",
+    'Milagro en la calle 34',
+    'Una historia de Navidad',
+    'Un villancico'
+  ],
+  answer: "Jingle All the Way",
+  answerES: "Resuena todo el camino",
+  img: 'jinglealltheway.png'
 }
-,{
-    question: "In Charles Dickens' A Christmas Carol, what was Mr. Scrooge's first name?",
-    choices: [
-        "Ebenezer",
-        'Gabriel',
-        'Charles',
-        'Hansel'
-    ],
-    answer: "Ebenezer",
-    img: 'tinytim.png'
+  , {
+  question: "In Charles Dickens' A Christmas Carol, what was Mr. Scrooge's first name?",
+  question: "En A Christmas Carol, de Charles Dickens, ¿cuál era el primer nombre del Sr. Scrooge?",
+  choices: [
+    "Ebenezer",
+    'Gabriel',
+    'Charles',
+    'Hansel'
+  ],
+  choicesES: [
+    "Ebenezer",
+    'Gabriel',
+    'Charles',
+    'Obsequiar'
+  ],
+  answer: "Ebenezer",
+  answerES: "Ebenezer",
+  img: 'tinytim.png'
 }
-,{
-    question: "After leaving Bethlehem, to which country did Joseph, Mary, and Jesus travel?",
-    choices: [
-        "Egypt",
-        'Bethlehem',
-        'Nazareth',
-        'Jerusalem'
-    ],
-    answer: "Egypt",
-    img: 'travel-Mary-and-Joseph.jpg'
+  , {
+  question: "After leaving Bethlehem, to which country did Joseph, Mary, and Jesus travel?",
+  questionES: "Después de dejar Belén, ¿a qué país viajaron José, María y Jesús?",
+  choices: [
+    "Egypt",
+    'Bethlehem',
+    'Nazareth',
+    'Jerusalem'
+  ],
+  choicesES: [
+    "Egipto",
+    'Belén',
+    'Nazaret',
+    'Jerusalén'
+  ],
+  answer: "Egypt",
+  answerES: "Egipto",
+  img: 'travel-Mary-and-Joseph.jpg'
 }
-,{
-    question: "Christmas is celebrated on the 25th of December. What is celebrated on the 26th of December?",
-    choices: [
-        "Boxing Day",
-        'Family Day',
-        'Thanksgiving',
-        'Three Kings Day'
-    ],
-    answer: "Boxing Day",
-    img: 'theater.png'
+  , {
+  question: "Christmas is celebrated on the 25th of December. What is celebrated on the 26th of December?",
+  questionES: "La Navidad se celebra el 25 de diciembre. ¿Qué se celebra el 26 de diciembre?",
+  choices: [
+    "Boxing Day",
+    'Family Day',
+    'Thanksgiving',
+    'Three Kings Day'
+  ],
+  choicesES: [
+    "Día del boxeo",
+    'Dia familiar',
+    'Acción de gracias',
+    'Dia de los tres Reyes'
+  ],
+  answer: "Boxing Day",
+  answerES: "Día del boxeo",
+  img: 'theater.png'
 }
-,{
-    question: "How many points does a snowflake traditionally have?",
-    choices: [
-        "6",
-        '8',
-        '5',
-        '4'
-    ],
-    answer: "6",
-    img: 'snow.png'
+  , {
+  question: "How many points does a snowflake traditionally have?",
+  questionES: "¿Cuántos puntos tiene tradicionalmente un copo de nieve?",
+  choices: [
+    "6",
+    '8',
+    '5',
+    '4'
+  ],
+  answer: "6",
+  img: 'snow.png'
 }
-,{
-    question: "Which was the first state in the United States to recognize Christmas as an official holiday?",
-    choices: [
-        "Alabama",
-        'New York',
-        'Florida',
-        'Massachusetts'
-    ],
-    answer: "Alabama",
-    img: 'xmas_lead_variante_02.gif'
+  , {
+  question: "Which was the first state in the United States to recognize Christmas as an official holiday?",
+  questionES: "¿Cuál fue el primer estado en los Estados Unidos en reconocer la Navidad como un día festivo oficial?",
+  choices: [
+    "Alabama",
+    'New York',
+    'Florida',
+    'Massachusetts'
+  ],
+  choicesES: [
+    "Alabama",
+    'Nueva York',
+    'Florida',
+    'Massachusetts'
+  ],
+  answer: "Alabama",
+  answerES: "Alabama",
+  img: 'xmas_lead_variante_02.gif'
 }
-,{
-    question: "Thomas Edison in 1880 invented electric Christmas lights?",
-    choices: [
-        'True',
-        'False'
-    ],
-    answer: 'True',
-    img: 'lights.jpg'
+  , {
+  question: "Thomas Edison in 1880 invented electric Christmas lights?",
+  questionES: "¿Thomas Edison en 1880 inventó las luces navideñas eléctricas?",
+  choices: [
+    'True',
+    'False'
+  ],
+  choicesES: [
+    'Cierto',
+    'Falso'
+  ],
+  answer: 'True',
+  answerES: 'Cierto',
+  img: 'lights.jpg'
 }
-,{
-    question: "Before turkey, the traditional English Christmas dinner included a pig’s head smothered in mustard.",
-    choices: [
-        'True',
-        'False'
-    ],
-    answer: 'True',
-    img: 'turkey.jpg'
+  , {
+  question: "Before turkey, the traditional English Christmas dinner included a pig’s head smothered in mustard.",
+  questionES: "Antes del pavo, la tradicional cena de Navidad inglesa incluía una cabeza de cerdo sofocada en mostaza.",
+  choices: [
+    'True',
+    'False'
+  ],
+  choicesES: [
+    'Cierto',
+    'Falso'
+  ],
+  answer: 'True',
+  answerES: 'Cierto',
+  img: 'turkey.jpg'
 }
-,{
-   question: "What is the name of this song?",
-   choices: [
-        'We wish you a merry christmas',
-        'Jingle Bells',
-        'OH Holy Night',
-        'O Christmas Tree'
-    ],
-    answer: 'We wish you a merry christmas',
-    img: 'Christmas-Song.png',
-    music: 'we-wish-you-a-merry-christmas.mp3',
-    timeToAnswerMs: 20000
+  , {
+  question: "What is the name of this song?",
+  questionES: "¿Cuál es el nombre de esta canción?",
+  choices: [
+    'We wish you a merry christmas',
+    'Jingle Bells',
+    'OH Holy Night',
+    'O Christmas Tree'
+  ],
+  choicesES: [
+    'Le deseamos una Feliz Navidad',
+    'Jingle Bells',
+    'Oh noche santa',
+    'O árbol de navidad'
+  ],
+  answer: 'We wish you a merry christmas',
+  answerES: 'Le deseamos una Feliz Navidad',
+  img: 'Christmas-Song.png',
+  music: 'we-wish-you-a-merry-christmas.mp3',
+  timeToAnswerMs: 20000
 }
-,{
-   question: "What is the name of this song?",
-   choices: [
-        'Angels We Have Heard on High',
-        'The First Noel',
-        'OH Holy Night',
-        'Joy to the World'
-    ],
-    answer: 'Angels We Have Heard on High',
-    img: 'Christmas-Song.png',
-    music: 'angels-we-have-heard.mp3',
-    timeToAnswerMs: 20000
+  , {
+  question: "What is the name of this song?",
+  questionES: "¿Cuál es el nombre de esta canción?",
+  choices: [
+    'Angels We Have Heard on High',
+    'The First Noel',
+    'OH Holy Night',
+    'Joy to the World'
+  ],
+  choicesES: [
+    'Ángeles que hemos escuchado en las alturas',
+    'El primer Noel',
+    'Oh noche santa',
+    'Alegría para el mundo'
+  ],
+  answer: 'Angels We Have Heard on High',
+  answerES: 'Ángeles que hemos escuchado en las alturas',
+  img: 'Christmas-Song.png',
+  music: 'angels-we-have-heard.mp3',
+  timeToAnswerMs: 20000
 }
-,{
-   question: "What is the name of this song?",
-   choices: [
-        'Joy to the World',
-        'Jingle Bells',
-        'OH Holy Night',
-        'Angels We Have Heard on High'
-    ],
-    answer: 'Joy to the World',
-    img: 'Christmas-Song.png',
-    music: 'joy-to-the-world.mp3',
-    timeToAnswerMs: 20000
+  , {
+  question: "What is the name of this song?",
+  questionES: "¿Cuál es el nombre de esta canción?",
+  choices: [
+    'Joy to the World',
+    'Jingle Bells',
+    'OH Holy Night',
+    'Angels We Have Heard on High'
+  ],
+  choicesES: [
+    'Alegría para el mundo',
+    'Jingle Bells',
+    'Oh noche santa',
+    'Ángeles que hemos escuchado en las alturas'
+  ],
+  answer: 'Joy to the World',
+  answerES: 'Alegría para el mundo',
+  img: 'Christmas-Song.png',
+  music: 'joy-to-the-world.mp3',
+  timeToAnswerMs: 20000
 }
 ];
